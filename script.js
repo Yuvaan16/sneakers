9668bf91e570bc9749fd6fb24eaf4e7abaa0cdad
Yuvaan Mutreja <67225353+Yuvaan16@users.noreply.github.com>
2022-08-13 16:43:56 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+$(function(){
+  $("#nav-placeholder").load("nav.html");
+});
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
+    var y = document.getElementById("logo")
     if (x.className === "topnav") {
       x.className += " responsive";
+      y.className += "responsive";      
     } else {
       x.className = "topnav";
+      y.className = "active";
     }
   }
